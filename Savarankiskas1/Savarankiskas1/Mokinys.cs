5355dac37b37b3a41195faf64bf92f74311036e5
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-17 20:00:00 +0200

    Datos

---
+++ Savarankiskas1/Savarankiskas1/Mokinys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas1
+{
+    struct Mokinys
+    {
+        public string vardas;
+        public string pavarde;
+        public DateTime kursoPradzia;
+        public DateTime kursoPabaiga;
+    }
+}
---
+++ Savarankiskas1/Savarankiskas1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mokinys petras = new Mokinys();
+            petras.vardas = "Petras";
+            petras.pavarde = "Petraitis";
+            petras.kursoPradzia = new DateTime(2019, 12, 4);
+            petras.kursoPabaiga = new DateTime(2020, 02, 25);
+
+            Console.WriteLine("{0} {1}, Kurso Pradzia: {2}, Kurso pabaiga: {3}", petras.vardas, petras.pavarde, petras.kursoPradzia.ToShortDateString(), petras.kursoPabaiga.ToLongDateString());
+            Console.ReadLine();
+        }
+    }
+}
