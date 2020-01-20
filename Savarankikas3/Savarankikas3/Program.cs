22c8b502240e8c2d099fe65d2228d2b9af5b7723
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-20 13:56:22 +0200

    Kazkas neveikia

---
+++ Savarankikas3/Savarankikas3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankikas3
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Staciakampis staciakampis1 = new Staciakampis(1, 5, 4);
+            Staciakampis staciakampis2 = new Staciakampis(2, 6, 6);
+            Staciakampis staciakampis3 = new Staciakampis(3, 3, 2);
+
+            staciakampis1.ApskaiciuotiStaciakampioPlota();
+            staciakampis2.ApskaiciuotiStaciakampioPlota();
+            staciakampis3.ApskaiciuotiStaciakampioPlota();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Savarankikas3/Savarankikas3/staciakampis.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankikas3
+{
+    struct Staciakampis
+    {
+        public int Staciakampiai;
+        public double Ilgis;
+        public double Plotis;
+        
+
+        public Staciakampis(int staciakampis, double ilgis, double plotis)
+        {
+            Staciakampiai = staciakampis;
+            Ilgis = ilgis;
+            Plotis = plotis;
+
+        }
+        public void ApskaiciuotiStaciakampioPlota()
+        {
+            Console.WriteLine("Staciakampi {staciakampis} plotas yra: {ilgis * plotis}");
+        }
+    }
+}
+class Class1
+    {
+    }
+
