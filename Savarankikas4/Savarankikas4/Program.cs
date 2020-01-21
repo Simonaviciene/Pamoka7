1531d363b07b4ca8ca1074e72525423d007a53a8
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-21 12:37:09 +0200

    nebaigtas

---
+++ Savarankikas4/Savarankikas4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankikas4
+{
+   
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        List<Studentas> studentuSarasas = new List<Studentas>();
+            studentuSarasas.Add(new Studentas(1));
+            studentuSarasas.Add(new Studentas(2));
+            studentuSarasas.Add(new Studentas(3));
+            studentuSarasas.Add(new Studentas(4));
+            studentuSarasas.Add(new Studentas(5));
+
+           
+
+        }
+    }
+}
---
+++ Savarankikas4/Savarankikas4/Studentas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankikas4
+{
+    struct Studentas
+    {
+        public int StudentoID;
+        public bool IskaitosRezultatas;
+
+        public Studentas(int id, bool rezult)
+        {
+            StudentoID = id;
+            IskaitosRezultatas = rezult;
+
+        public override string ToString()
+        {
+            return("ID: " + StudentoID + "   Iskaitos Rezultatas: " + IskaitosRezultatas);
+        }
+        
+        }
+    }
+}   
